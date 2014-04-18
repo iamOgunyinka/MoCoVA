e6275f1adea1f5b3962effea6abbbfa2020bc391
Josh <ogunyinkajoshua@yahoo.com>
2014-04-18 13:01:02 +0800

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,20 @@
+#IFNDEF PARSER_H_
+#define PARSER_H
+
+#include <QSyntaxHighlighter>
+#include <QWidget>
+#include <QWidgets>
+
+class Parser: public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Parser(QWidget *parent = nullptr);
+
+public slots:
+
+};
+
+
+
+#endif
